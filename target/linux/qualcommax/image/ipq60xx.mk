7e83294c6bdcdc421940da4e4b152d9b8c06b0ff
277598414 <277598414@qq.com>
2025-03-20 12:31:16 +0800

    Update ipq60xx.mk

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -1,3 +1,9 @@
+define Device/EmmcImage
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+
 define Device/8devices_mango-dvk
 	$(call Device/FitImageLzma)
 	DEVICE_VENDOR := 8devices
@@ -14,12 +20,6 @@ TARGET_DEVICES += 8devices_mango-dvk
 define Device/EmmcImage
 	IMAGES += factory.bin sysupgrade.bin
 	IMAGE/factory.bin := append-kernel | pad-to 6144k | append-rootfs | append-metadata
-	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
-endef
-
-define Device/EmmcImage
-	IMAGES += factory.bin sysupgrade.bin
-	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
 	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
 endef
 
@@ -75,12 +75,9 @@ define Device/jdc_ax1800-pro
 	DEVICE_DTS_CONFIG := config@cp03-c2
 	DEVICE_DTS := ipq6000-jdc-ax1800-pro
 	SOC := ipq6000
-	DEVICE_PACKAGES := ipq-wifi-jdc_ax1800-pro
+	DEVICE_PACKAGES := ipq-wifi-jdc_ax1800-pro kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
 	BLOCKSIZE := 64k
 	KERNEL_SIZE := 6144k
-	IMAGES += kernel.bin rootfs.bin factory.bin
-	IMAGE/kernel.bin := append-kernel
-	IMAGE/rootfs.bin := append-rootfs | pad-rootfs | pad-to $$(BLOCKSIZE)
 	IMAGE/factory.bin := append-kernel | pad-to $$$${KERNEL_SIZE}  |  append-rootfs | append-metadata
 endef
 TARGET_DEVICES += jdc_ax1800-pro
