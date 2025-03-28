af1aafa9394002e93a4aaa9cbd1c27b41682d8b2
277598414 <277598414@qq.com>
2025-03-28 16:31:23 +0800

    Add files via upload

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -13,6 +13,12 @@ define Device/8devices_mango-dvk
 endef
 TARGET_DEVICES += 8devices_mango-dvk
 
+define Device/EmmcImage
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+
 define Device/cambiumnetworks_xe3-4
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
@@ -53,6 +59,21 @@ define Device/glinet_gl-axt1800
 	SUPPORTED_DEVICES += glinet,axt1800
 endef
 TARGET_DEVICES += glinet_gl-axt1800
+
+define Device/jdc_ax1800-pro
+	$(call Device/FitImage)
+	$(call Device/EmmcImage)
+	DEVICE_VENDOR := JD Cloud
+	DEVICE_MODEL := JDC AX1800 Pro
+	DEVICE_DTS_CONFIG := config@cp03-c2
+	DEVICE_DTS := ipq6000-jdc-ax1800-pro
+	SOC := ipq6000
+	DEVICE_PACKAGES := ipq-wifi-jdc_ax1800-pro kmod-fs-ext4 mkf2fs f2fsck kmod-fs-f2fs
+	BLOCKSIZE := 64k
+	KERNEL_SIZE := 6144k
+	IMAGE/factory.bin := append-kernel | pad-to $$$${KERNEL_SIZE}  |  append-rootfs | append-metadata
+endef
+TARGET_DEVICES += jdc_ax1800-pro
 
 define Device/linksys_mr7350
 	$(call Device/FitImage)
